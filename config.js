686a17f28124006670acbcdf859b64d4da081408
Ben Allred <skuttleman@gmail.com>
2016-09-17 22:25:42 -0600

    A little easier to configure

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  lyrics: function() {
+    return [
+      { lyric: 'Push it.', timeout: 785 },
+      { lyric: 'Push it good.', timeout: 2725 },
+      { lyric: 'Push it.', timeout: 4725 },
+      { lyric: 'Push', timeout: 6000 },
+      { lyric: 'it', timeout: 6250 },
+      { lyric: 'REAL', timeout: 6525 },
+      { lyric: 'GOOD!', timeout: 7000 }
+    ];
+  },
+  playMusic: function() {
+    return 'afplay ~/.git-push-it/pushit.mp3';
+  },
+  gitPush: function(remote, branch) {
+    return ['git push', remote, branch].join(' ');
+  }
+};
